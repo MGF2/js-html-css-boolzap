672436db9344d96c66ca201584f5871bf8814eb1
Unknown <fioremartina@hotmail.com>
2020-07-30 13:30:07 +0200

    main js almost fixed

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
-// var msg = $('template').clone();
-// msg.addClass('sent');
-// $('.main-conv').append(msg);
 
-$('#content').keydown(invio);
+$("#content").keydown(function(){
+    if (event.which == 13 || event.keyCode == 13) {
+        invio();
 
-function invio(event) {
-  if(event.which == 13 || event.keyCode == 13){
-    //prendiamo il contenuto
-    var contenuto = $('#content').val();
-    console.log(contenuto);
+    }
+});
 
-    var msg = $('.template').clone();
-    var text = $('.template p');
+function invio(){
+    // 1 step -- prendere il valore
+    var valore = $("#content").val();
+    // 2 step -- creo clone del template
+    //creo clone del template partendo da message che si trova nella classe message
+    var clone = $(".template .message").clone();
+    // 3 STEP -- Aggiungo una classe
+    clone.addClass("sent");
+    // 4 step -- inserimento del testo
+    clone.find(".cloud-top p").append(valore);
+    clone.find(".cloud-time small").append("16.36");
+    // 5 step - Inserimento nel DOM
+    $(".main-conv").append(clone);
+    // 6 step - risposta automatica
+    setTimeout(rispostaAutomatica,2000);
+    // 7 step -reset input
+    valore = $("#content").val('');
+}
 
-    console.log(msg);
-    console.log(text);
-
-    msg.removeClass('display');
-    msg.show();
-    text.text(contenuto);
-    // msg.append(contenuto);
-
-    msg.addClass('sent');
-    // msg.addClass('cloud');
-
-    // msg.show();
-
-    $('.main-conv').append(msg);
-  }
+function rispostaAutomatica(){
+    // 1 step - clonare il template
+    // message dove parte il clone
+    var clone = $(".template .message").clone();
+    // 2 step - mettere la classe
+    clone.addClass("received");
+    // 3 step - aggiungere il Testo
+    clone.find(".cloud-top p").append("Ok");
+    clone.find(".cloud-time small").append('15.30');
+    // 4 step - mettere elemento nel DOM
+    $(".main-conv").append(clone);
 }
